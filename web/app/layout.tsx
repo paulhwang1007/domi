99bb88bbd82eff86eaa0ffb4789de1debcf41005
Paul Hwang <hwpaul1007@gmail.com>
2026-01-07 22:35:48 -0800

    Enable Vercel Web Analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,6 +33,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Analytics />
         </ThemeProvider>
       </body>
     </html>
